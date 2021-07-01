17712e03684149a86cd3dc86b921ac8c6d0cc433
n232not <71568363+n232not@users.noreply.github.com>
2021-06-30 17:48:07 -0700

    Stacked Bar visualization code

--- visualization.R
+++ visualization.R
@@ -13,36 +13,44 @@ data1 <- y %>%
 data2 <- z %>% 
   group_by(Step) %>% 
   summarise(across(everything(), sum))
-data1 = as.data.frame(data1)
-data2 = as.data.frame(data2)
-data1 <- data1[,colSums(data1)>0]
-data2 <- data2[,colSums(data2)>0]
+data1 <- as.data.frame(data1)
+data2 <- as.data.frame(data2)
+data1 <- data1[, colSums(data1 != 0) > 0]
+data2 <- data2[, colSums(data2 != 0) > 0]
 data1[,1] = rep(0:9, each=4)
 data2[,1] = c(0:9)
-long_df <- data1 %>% gather(Key, Value, -Step,Species)
-long_df_2 <- data2 %>% gather(Key, Value, -Step,Species)
-print.data.frame(data2)
-long_df = long_df %>%
+long_df <- data1 %>% gather(Key, Value, -Step, -Species)
+long_df_2 <- data2 %>% gather(Key, Value, -Step)
+print.data.frame(long_df_2)
+long_df_2 = long_df_2 %>%
   group_by(Step)%>%      
   mutate(rank = rank(-Value, ties.method = 'first'),
          label = paste0(" ", Value)) %>%
   group_by(Value)
-print.data.frame(long_df)
-anim = ggplot(data=long_df, mapping=aes(x=rank, y=Value, fill=Key, frame = Step))+
-        geom_bar(stat = "identity") +
+long_df_2 = long_df_2[,-3]
+total <- merge(long_df,long_df_2,by=c("Step","Key"))
+print.data.frame(total)
+anim = ggplot(data=total, mapping=aes(x=rank, y=Value, fill=Species, frame = Step))+
+        geom_bar(stat = "identity", position = "stack") +
         geom_text(aes(y = 0, label = Key, vjust = 0, hjust = 1.1, size = 1, angle = 90)) +
-        geom_text(aes(y = Value, label = label, hjust = 0, size = 1, angle = 90)) +
+        geom_text(aes(y = as.integer(sub('.', '', label)), label = label, hjust = 0, size = 1, angle = 90)) +
         labs(y = "Population Infected", x="Steps") +
         ggtitle("Virus Strains Over Time") +
-        theme(legend.position = "none",
+        guides(size = FALSE) +
+        theme(legend.position = c(0.9, 0.8),
+              legend.key.size = unit(1, 'cm'), #change legend key size
+              legend.key.height = unit(1, 'cm'), #change legend key height
+              legend.key.width = unit(1, 'cm'), #change legend key width
+              legend.title = element_text(size=16, family = "Rockwell"), #change legend title font size
+              legend.text = element_text(size=14, family = "Rockwell Condensed"),
               axis.text.x=element_blank(),
               axis.text.y=element_blank(),
               axis.ticks=element_blank(),
-              axis.title.y=element_text(vjust=-7, hjust=0.5, size=25, family = "Franklin Gothic Demi"),
-              axis.title.x=element_text(vjust=1, hjust=0.5, size=25, family = "Franklin Gothic Demi"),
+              axis.title.y=element_text(vjust=-7, hjust=0.5, size=25, family = "Rockwell Condensed"),
+              axis.title.x=element_text(vjust=1, hjust=0.5, size=25, family = "Rockwell Condensed"),
               panel.background=element_blank(),
               panel.border=element_blank(),
-              plot.title = element_text(size=50, vjust=-2, hjust=0.5, family = "Rockwell"),
+              plot.title = element_text(size=50, vjust=-2, hjust=0.5, family = "Rockwell Extra Bold"),
               plot.background=element_blank(),
               panel.grid.major=element_blank(),
               panel.grid.minor=element_blank(),
@@ -50,6 +58,6 @@ anim = ggplot(data=long_df, mapping=aes(x=rank, y=Value, fill=Key, frame = Step)
         transition_states(Step, transition_length = 1, state_length = 1) +
         ease_aes('sine-in-out')
 
-animate(anim, nframes = 10,fps = 1,  width = 1200, height = 850, 
-        renderer = gifski_renderer("strains.gif"))
+animate(anim, nframes = 600,fps = 40,  width = 1200, height = 850, 
+        renderer = gifski_renderer("animated_bar.gif"))
 
